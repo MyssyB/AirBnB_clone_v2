0e95ec1e26feb8003facbe530c6f747daaf451da
MyssyB <beulahagiande@gmail.com>
2024-07-30 08:00:54 -0700

    update flask

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Flask script to start a web app"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Say something on start
+    listening: 0.0.0.0
+    port: 5000
+    """
+
+    return "Hello HBNH!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """HBNB output"""
+    return "HBNB"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0' port=5000)
