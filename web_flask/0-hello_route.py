d8c1e54974126392f0afcd0c3d5270d9c528aa38
MyssyB <beulahagiande@gmail.com>
2024-07-30 06:54:23 -0700

    flask first commit

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""flask script to start a web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Say something on start
+    listening: 0.0.0.0
+    port: 5000
+    """
+
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5000)
